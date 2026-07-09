06709619a04610f8f6430fa402eeed2ff61e74d3
dodclakmuthu <markdot1024@gmail.com>
2026-07-09 23:27:13 +0530

    fix default port to 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const os = require("os");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 function getServerIp() {
   const interfaces = os.networkInterfaces();
